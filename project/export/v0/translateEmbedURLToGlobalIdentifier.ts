5025d3dba4f0f0f0a5ae9f29a60bb42227728f9f
eXory2024 <exory@anio.software>
2025-07-15 05:46:45 +0200

    Use parseEmbedURL from spec package.

--- project/export/v0/translateEmbedURLToGlobalIdentifier.ts
+++ project/export/v0/translateEmbedURLToGlobalIdentifier.ts
@@ -1,12 +1,12 @@
 import type {EnkoreJSRuntimeProjectAPIContext} from "@anio-software/enkore-private.spec"
-import {_parseEmbedURLRemoveMeAfterUpdate} from "./_parseEmbedURLRemoveMeAfterUpdate.ts"
+import {parseEmbedURL} from "@anio-software/enkore-private.spec"
 
 export function translateEmbedURLToGlobalIdentifier(
 	context: EnkoreJSRuntimeProjectAPIContext,
 	embedURL: string
 ): string {
 	const {name, version} = context.project.packageJSON
-	const {protocol, path} = _parseEmbedURLRemoveMeAfterUpdate(embedURL)
+	const {protocol, path} = parseEmbedURL(embedURL)
 
 	return `${name}/v${version}/${protocol}/${path}`
 }
